0993f6513449a59dd755f761517826c5a34a9b39
Hazem-awy <hazemhamdy2234@gmail.com>
2024-09-17 19:56:20 +0300

    category

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const companyRoutes = require('./routes/companyRoutes');
 const customerRoutes = require('./routes/customerRoutes');
 const brandRoutes = require('./routes/brandRoutes');
+const categoryRoutes= require('./routes/categoryRoutes');
 const app = express();
 app.use(express.json());
 
 app.use('/api', companyRoutes);
 app.use('/api', customerRoutes);
 app.use('/api', brandRoutes);
+app.use('/api', categoryRoutes);
 
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
